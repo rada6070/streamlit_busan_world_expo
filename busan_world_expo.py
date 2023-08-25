db5a9cee76154470cc8582d8bd79d60fc22b6434
rada6070 <141891268+rada6070@users.noreply.github.com>
2023-08-25 15:26:54 +0900

    Update busan_world_expo.py

--- busan_world_expo.py
+++ busan_world_expo.py
@@ -42,6 +42,8 @@ page_1_explain_when = "날짜: 2030년 5월 1일 - 10월 31일"
 page_1_explain_where = "장소: 부산, 대한민국"
 page_1_explain_thema = "테마: '새로운 세계를 여는 혁신과 지속가능성'"
 
+page_1_image_gall = "이미지 갤러리"
+
 page_1_subheader_2 = "더 알아보기"
 page_1_explain_2 = "공식 웹사이트에서 더 많은 정보를 확인하세요:"
 
@@ -88,7 +90,61 @@ with st.sidebar:
 
 st.sidebar.image("https://i.namu.wiki/i/tvGyhYywWMsAcu6DB_LNqDgXsPeaXzDt4Su8mc8pckqINu1ceRlXh6mqVaquCFE9vCBk9Pduf9xkzWr0gcC_Ng.svg", use_column_width=True)
 
-st.sidebar.selectbox( 'lang select' , sl_box )
+sl_sc = st.sidebar.selectbox( 'lang select' , sl_box )
+
+if sl_sc == 'English' :
+        global side_bar_main
+        global side_bar_sub_1
+        global side_bar_sub_2
+        global side_bar_sub_3
+
+        global page_1_title
+        global page_1_explain_1
+
+        global page_1_subheader_1
+        global page_1_explain_when
+        global page_1_explain_where
+        global page_1_explain_thema
+
+        global page_1_image_gall
+
+        global page_1_subheader_2
+        global page_1_explain_2
+
+        global page_2_title
+
+        global page_3_title
+        global page_3_columns_1
+        global page_3_columns_2
+        global page_3_columns_3
+        global page_3_columns_4
+
+        side_bar_main = "Busan 2030 World Expo"
+        side_bar_sub_1 = "Promotion of Busan World Expo"
+        side_bar_sub_2 = "Map of Busan"
+        side_bar_sub_3 = "Busan Tourist Site Preference Graph"
+
+        page_1_title = "Promotion of Busan World Expo"
+        page_1_explain_1 = "Busan World Expo 2030 is an international event that displays cultures and technologies from around the world."
+
+        page_1_subheader_1 = "Key Event Information"
+        page_1_explain_when = "Date: May 1, 2030 - October 31, 2030"
+        page_1_explain_where = "Location: Busan, South Korea"
+        page_1_explain_thema = "Theme: 'Innovation and Sustainability Opening a New World'"
+
+        page_1_image_gall = "Image Gallery"
+
+        page_1_subheader_2 = "Learn more information"
+        page_1_explain_2 = "Check out the official website for more information:"
+
+        page_2_title = "Map of Busan"
+
+        page_3_title = "Busan Tourist Site Preference Graph"
+        page_3_columns_1 = "Gamcheon Culture Village"
+        page_3_columns_2 = "Jagalchi Market"
+        page_3_columns_3 = "International Market"
+        page_3_columns_4 = "Bujeon Market"
+        
 
 if choose == side_bar_sub_1 :
     # 페이지 제목과 부산 월드 엑스포 2030 로고 이미지 표시
@@ -96,16 +152,16 @@ if choose == side_bar_sub_1 :
     st.image("https://image.edaily.co.kr/images/Photo/files/NP/S/2022/06/PS22062300100.jpg", use_column_width=True)
 
     # 월드 엑스포 2030에 대한 간단한 설명 추가
-    st.write("부산 월드 엑스포 2030은 세계 각국의 문화와 기술을 전시하는 국제 행사입니다.")
+    st.write(page_1_explain_1)
 
     # 주요 행사 정보 표시
-    st.subheader("주요 행사 정보")
-    st.write("날짜: 2030년 5월 1일 - 10월 31일")
-    st.write("장소: 부산, 대한민국")
-    st.write("테마: '새로운 세계를 여는 혁신과 지속가능성'")
+    st.subheader(page_1_subheader_1)
+    st.write(page_1_explain_when)
+    st.write(page_1_explain_where)
+    st.write(page_1_explain_thema)
 
     # 부산 월드 엑스포 2030 관련 이미지 표시
-    st.subheader("이미지 갤러리")
+    st.subheader(page_1_image_gall)
     image_urls = [
         "https://thumb.mt.co.kr/06/2021/11/2021110114172440715_1.jpg/dims/optimize/https://thumb.mt.co.kr/06/2021/11/2021110114172440715_1.jpg/dims/optimize/",
         "https://thumb.mtstarnews.com/06/2023/04/2023040312452334740_1.jpg/dims/optimizehttps://thumb.mtstarnews.com/06/2023/04/2023040312452334740_1.jpg/dims/optimize",
@@ -116,12 +172,12 @@ if choose == side_bar_sub_1 :
     st.image(image_urls, width=200)
 
     #월드 엑스포에 대한 정보를 더 자세히 보여주는 링크 제공
-    st.subheader("더 알아보기")
-    st.write("공식 웹사이트에서 더 많은 정보를 확인하세요:")
+    st.subheader(page_1_subheader_2)
+    st.write(page_1_explain_2)
     st.write("[www.expo2030busan.kr](https://www.expo2030busan.kr/index.do)")
 
-if choose == "부산의 지도" :
-    st.title('부산의 지도') 
+if choose == side_bar_sub_2 :
+    st.title(side_bar_sub_2) 
     ## 위도와 경도 값을 가진 샘플 DataFrame 생성
     data = pd.DataFrame({
         'latitude': [35.1795543],
@@ -131,10 +187,10 @@ if choose == "부산의 지도" :
     ## 데이터를 사용하여 지도 생성
     st.map(data)
 
-if choose == "부산 관광지 선호도 그래프" :
-    st.title('부산 관광지 선호도 그래프')
+if choose == side_bar_sub_3 :
+    st.title(side_bar_sub_3)
     chart_data = pd.DataFrame(
         np.random.randn(20, 4),
-        columns=['감천 문화 마을', '자갈치 시장', '국제 시장', '부전 시장'])
+        columns=[page_3_columns_1, page_3_columns_2, page_3_columns_3, page_3_columns_4])
 
     st.bar_chart(chart_data)
