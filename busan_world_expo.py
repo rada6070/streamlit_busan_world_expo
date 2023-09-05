96c9827a11a8ffb54afc8d2c12df2107a0de1805
rada6070 <141891268+rada6070@users.noreply.github.com>
2023-09-05 10:48:21 +0900

    Update busan_world_expo.py

--- busan_world_expo.py
+++ busan_world_expo.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_folium import st_folium
+import folium
 from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
 from  PIL import Image
@@ -6,7 +8,6 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import io
-import folium
 
 #언어 셀렉트 박스 변수
 
