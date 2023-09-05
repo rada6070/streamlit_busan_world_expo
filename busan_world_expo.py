232ba419b3ad523ea5afe3032cf9f56543a61c3b
rada6070 <141891268+rada6070@users.noreply.github.com>
2023-09-05 21:51:11 +0900

    Update busan_world_expo.py

--- busan_world_expo.py
+++ busan_world_expo.py
@@ -244,6 +244,7 @@ if choose == side_bar_sub_2 :
     df = pd.DataFrame({
         'latitude': [37.7749, 34.0522, 40.7128],
         'longitude': [-122.4194, -118.2437, -74.0060]
+    
     st.map(df)
     # 지도 생성 
 
