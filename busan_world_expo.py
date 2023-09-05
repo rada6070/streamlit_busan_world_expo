512dc6a7db180d3be94113475b9379c264b92d05
rada6070 <141891268+rada6070@users.noreply.github.com>
2023-09-05 12:51:23 +0900

    Update busan_world_expo.py

--- busan_world_expo.py
+++ busan_world_expo.py
@@ -240,7 +240,26 @@ if choose == side_bar_sub_1 :
 
 if choose == side_bar_sub_2 :
     st.title(side_bar_sub_2) 
-    st.map()
+    #지도 위에 표시될 점 좌표 값을 위도경도에 담습니다 .
+base_position =  [37.5073423, 127.0572734]
+#중심점의 위도, 경도 좌표를 리스트에 담습니다.
+
+# base_position에, 랜덤으로 생성한 값을 더하여 5개의 좌표를 데이터 프레임으로 생성하였고,
+# 컬럼명은 위도 :lat  경도 lon으로 지정하였습니다. 
+
+
+map_data = pd.DataFrame(
+    np.random.randn(5, 1) / [20, 20] + base_position , 
+    columns=['lat', 'lon'])
+    # map data 생성 : 위치와 경도 
+
+
+    st.code('st.map(map_data)')
+    # 웹사이트에 어떤 코드인지 표시해주기 
+    st.subheader('Map of Data ')
+    # 제목 생성 
+    st.map(map_data)
+    # 지도 생성 
 
 if choose == side_bar_sub_3 :
     st.title(side_bar_sub_3)
