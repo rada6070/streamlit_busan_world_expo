2118d6b117da7bf91f5dfe3a20a8650d018ff73f
rada6070 <141891268+rada6070@users.noreply.github.com>
2023-09-24 18:38:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from streamlit_js_eval import get_browser_language
 st.markdown("<h1 style='text-align: center; color: black;'>부산 EXPO 챗봇 </h1>", unsafe_allow_html=True)
 
 # 디자인 칸 2개로 분리
-col1, col2= st.columns([1,4])
+col1, col2= st.columns([1,1])
 
 # 브라우저 기본 언어 불러오기
 try:
