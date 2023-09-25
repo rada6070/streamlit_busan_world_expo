08e532fe27c4437485db375565ec015af368c651
rada6070 <141891268+rada6070@users.noreply.github.com>
2023-09-26 02:00:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,12 +24,46 @@ from streamlit_folium import st_folium
 from streamlit_option_menu import option_menu
 from streamlit_js_eval import get_browser_language
 
+try:
+    preferred_language = i18n.FindLangByTag(get_browser_language())
+except AttributeError:
+    preferred_language = "ko"
+
+assets.import_assets("assets/style.css", st)
 
 
 st.markdown("<h1 style='text-align: center; color: black;'>부산 EXPO 관광지 추천 </h1>", unsafe_allow_html=True)
 
+col1, col2 = columns([1,1])
 
-df = pd.read_excel("data.xlsx")
+with col1 :
+    col11, col12, col13 = columns([1,6,1])
+
+    with col11 :
+        st.write(' ')
+
+    with col12 :
+        st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.facebook.com%2Fbusanworldexpo2030%2Fevents%2F%3Flocale%3Dda_DK&psig=AOvVaw3aKQWCRcP-dcUyxJ7seRvl&ust=1695747425465000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNjkjcmdxoEDFQAAAAAdAAAAABAk', width=85)
+
+    with col13 :
+        st.write(' ')
+
+with col2 :
+    language_list = list(i18n.LangList().keys())
+    language_select = str(
+        st.selectbox("Change Language", ["브라우저 기본 설정", *language_list])
+    )
+    
+    if language_select == "브라우저 기본 설정":
+        language = i18n.LoadLangByCode(preferred_language)
+    else:
+        language = i18n.LoadLangByCode(i18n.LangList(language_select))
+
+    
+
+
+df = pd.read_excel("C:/Users/USER/Desktop/data.xlsx")
+
 def bring_data():
     spots_df = df[df['분류'] == '엑스포']
     spots_df = spots_df.reset_index(drop=True)
@@ -44,6 +78,7 @@ Q, P, A, latitude, longitude = bring_data()
 
 f_btn_clicked = st.button(f'{Q[0]}', key='test_btn')
 n_btn_clicked = st.button('next', key='next_btn')
+b_btn_clicked = st.button('back', key='back_btn')
 
 i = st.session_state.get('i', 0)
 
@@ -55,13 +90,38 @@ if f_btn_clicked:
     st.write(f"위도: {latitude[i]}")
     st.write(f"경도: {longitude[i]}")
     
+    
 if n_btn_clicked:
-    i += 1
-    st.session_state['i'] = i
-    st.write(f"장소: {P[i]}")
-    st.write(f"답변: {A[i]}")
-    st.write(f"위도: {latitude[i]}")
-    st.write(f"경도: {longitude[i]}")
+    if i >= 4 :
+        i = 4
+        print('마지막입니다.')
+    elif i < 0 :
+        i = 0
+        st.warning('처음입니다.')
+    else :
+        i += 1
+        st.session_state['i'] = i
+        st.write(f"장소: {P[i]}")
+        st.write(f"답변: {A[i]}")
+        st.write(f"위도: {latitude[i]}")
+        st.write(f"경도: {longitude[i]}")
+
+    
+    
+if b_btn_clicked:
+    if i > 4 :
+        i = 4
+        st.warning('마지막입니다.')
+    if i <= 0 :
+        i = 0
+        st.warning('처음입니다.')
+    else:
+        i -= 1
+        st.session_state['i'] = i
+        st.write(f"장소: {P[i]}")
+        st.write(f"답변: {A[i]}")
+        st.write(f"위도: {latitude[i]}")
+        st.write(f"경도: {longitude[i]}")
 
 
 
